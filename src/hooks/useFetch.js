2bfa1a6f393d288fd0d7f7719e48737e2c68d9a2
camilita <camilaacevedorestrepo@gmail.com>
2025-03-14 18:43:13 -0500

    component finished (usefetch)

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export const useFetch=(url="")=>{
 
@@ -11,6 +11,37 @@ export const useFetch=(url="")=>{
 const getFetch = async()=>{
     setState({
         ...state,
+        isLoading: true,
+        hasError:null
     })
-}
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        setState({
+            ...SVGMetadataElement,
+            data,
+            isLoading:false,
+            hasError:null
+        })
+    } catch (error) {
+        setState({
+            ...state,
+            data:null,
+            isLoading:false,
+            hasError:error.message
+        });
+    }
+};
+
+    useEffect(()=>{
+        getFetch();
+    },[url]);
+    
+    return{
+        data: state.data,
+        isLoading: state.isLoading,
+        hasError: state.hasError
+    }
 }
